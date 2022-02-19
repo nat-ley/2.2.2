6a84259852968ec018a8c98824a6f6e2630be999
nat-ley <nat_ley@mail.ru>
2022-02-19 12:56:07 +0400

    2.2.2

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,9 +1,13 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +23,19 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping("/cars")
+	public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
+		List<Car> list = new ArrayList<>();
+		list.add(new Car("Ford", "RED", 500));
+		list.add(new Car("Ferrari", "YELLOW", 300));
+		list.add(new Car("Lada", "BLACK", 1000));
+		list.add(new Car("FIAT", "GREEN", 190));
+		list.add(new Car("BMW", "BLUE", 200));
+		list = CarService.carsCount(list, allCars);
+		model.addAttribute("cars_list", list);
+		return "cars";
 	}
 	
 }
