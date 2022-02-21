a7ecd23926e46e6d8dd8be20d6806d489ba5a1a4
nat-ley <nat_ley@mail.ru>
2022-02-21 19:58:28 +0400

    2.2.2

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -4,16 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
+	private final CarService carService;
+
+	public HelloController(CarService carService) {
+		this.carService = carService;
+	}
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -27,14 +32,7 @@ public class HelloController {
 
 	@GetMapping("/cars")
 	public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
-		List<Car> list = new ArrayList<>();
-		list.add(new Car("Ford", "RED", 500));
-		list.add(new Car("Ferrari", "YELLOW", 300));
-		list.add(new Car("Lada", "BLACK", 1000));
-		list.add(new Car("FIAT", "GREEN", 190));
-		list.add(new Car("BMW", "BLUE", 200));
-		list = CarService.carsCount(list, allCars);
-		model.addAttribute("cars_list", list);
+		model.addAttribute("cars_list", carService.carsCount(allCars));
 		return "cars";
 	}
 	
